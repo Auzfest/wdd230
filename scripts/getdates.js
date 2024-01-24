3acd6257749dfc8e7af6206ea72874bcfefa7711
Auzfest <114009523+Auzfest@users.noreply.github.com>
2024-01-24 16:07:06 -0700

    design principles, home page update, chamber plan started

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+var currentDate = new Date();
+
+// Format the date (you can customize the format as needed)
+var formattedDate = currentDate.getFullYear();
+
+// Set the formatted date as the content of the paragraph
+document.getElementById('dateParagraph').innerHTML = '@ ' + formattedDate;
