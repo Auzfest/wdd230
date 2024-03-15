21c648a17c1d485a35967d81c29384839e4cc822
Auzfest <114009523+Auzfest@users.noreply.github.com>
2024-03-15 11:20:23 -0600

    chamber update

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -1,10 +1,10 @@
-// select HTML elements in the document
 const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
 const describeWeather = document.querySelector('#describeWeather')
-const captionDesc = document.querySelector('figcaption');
+const weatherDiv = document.querySelector('.weather');
+const windSpeed = document.querySelector("#windSpeed");
 const url = 'https://api.openweathermap.org/data/2.5/forecast?appid=72084045ad798dbaa9b1eaa3a54a918a&lat=-30.035363832053992&lon=30.88894569159559&units=imperial';
-
+const currDate = new Date();
+const currDay = currDate.getDay();
 
 async function apiFetch() {
     try {
@@ -22,12 +22,47 @@ async function apiFetch() {
 
   
 function displayResults(data) {
-  forecastContainer.innerHTML = ''; // Clear previous forecast data
-  const forecasts = data.list.slice(0, 3); // Taking only the first 3 forecasts
+  const forecasts = data.list.slice(0, 3);
   forecasts.forEach(forecast => {
-      currentTemp.innerHTML = forecast.main.temp;
-      describeWeather.innerHTML = forecast.weather[0].description;
+      currentTemp.innerHTML = `${forecast.main.temp}&deg;F`;
+      describeWeather.innerHTML = `with ${forecast.weather[0].description}`;
+      windSpeed.innerHTML = `${forecast.wind.speed}`;
   });
+
+const windChill = calculateWindChill(currentTemp, windSpeed);
+document.getElementById('windChill').textContent = windChill;
+
+
+  for (let i = 1; i <= 3; i++) {
+    const forecast = data.list[i * 8];
+    const forecastTemp = forecast.main.temp;
+    const forecastDesc = forecast.weather[0].description;
+
+    // Update HTML for each forecast
+    const forecastElement = document.createElement('div');
+    forecastElement.innerHTML = `<p>${forecastTemp}&deg;F - ${forecastDesc}</p>`;
+    weatherDiv.appendChild(forecastElement);
   }
+}
+
+
+function calculateWindChill(temperature, windSpeed) {
+  if (temperature <= 50 && windSpeed >= 3) {
+      // Formula for wind chill factor
+      return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+  }
+  else {
+      return "N/A";
+    }
+}
   
-  apiFetch();
+apiFetch();
+
+if (currDay >= 1 && currDay <= 3) { // Monday, Tuesday, or Wednesday
+  document.getElementById('chamberBanner').style.display = 'grid';
+}
+
+// Function to close the banner
+function closeBanner() {
+  document.getElementById('chamberBanner').style.display = 'none';
+}
